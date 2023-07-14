ff1bfca374d9827fd0b6354b32109640a8ec56da
Erick Alan0123 <erickalan068@gmail.com>
2023-07-14 16:46:10 -0300

    feat(JUinit): conhecendo os metodos assertivos

--- src/main/java/br/ce/wcaquino/entidades/Usuario.java
+++ src/main/java/br/ce/wcaquino/entidades/Usuario.java
@@ -17,4 +17,33 @@ public class Usuario {
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((nome == null) ? 0 : nome.hashCode());
+		return result;
+	}
+
+	/*ao gerar o hashcode and equals, e criado esse metodo de comparacao entre atributo
+	ou seja, caracteristica para comparar se o objeto usuario e igual ao outro objeto. Voltando pra classe assert teste*/
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Usuario other = (Usuario) obj;
+		if (nome == null) {
+			if (other.nome != null)
+				return false;
+		} else if (!nome.equals(other.nome))
+			return false;
+		return true;
+	}
+	
+	
 }
---
+++ src/test/java/br/ce/wcaquino/servicos/AssertTest.java
@@ -0,0 +1,51 @@
+package br.ce.wcaquino.servicos;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import br.ce.wcaquino.entidades.Usuario;
+
+public class AssertTest {
+	
+	@Test
+	public void test() {
+		//onde deve retornar a condicao como verdadeira, 
+		//se o resultado for falso retorna o teste como errado
+		Assert.assertTrue(true);
+		//versao inversa
+		Assert.assertFalse(false);
+		//para ver se sao iguai (valorEsperado, valorAtual)
+		Assert.assertEquals(0, 0);
+		/*valores com numeros quebrados fica depreciado, ou seja, 
+		 * a comparacao(0.512, 0.512) nao fica totalmente certa. 
+		 * Pra isso, devemos inserir um medidor de casas decimais
+		 * para dizer ate que ponto a comparacao deve ser feita
+		 * exemplo (0.512, 0.512, 0.001)
+		 *  ele conta quantas casa depois do ponto sera comparado
+		 * 
+		 */
+		Assert.assertEquals(0.5122, 0.512, 0.001);
+		Assert.assertEquals(Math.PI, 3.14, 0.01);
+		/*valores do tipo primitivo(int,double,boolean...), nao da pra serem
+		 * comparadas com objetos(Integer, Double, Boolean....) 
+		 */
+		int primitivo = 0;
+		Integer objeto = 0;
+		Assert.assertEquals(Integer.valueOf(primitivo), objeto);//conversao de primitivo para objeto ou
+		Assert.assertEquals(primitivo, objeto.intValue());//conversao de objeto para primitivo
+		/* para comparacao string tem que ser exatamente igual*/
+		Assert.assertEquals("bola", "bola");
+		/*para string ignorar maiusculo e minusculo */
+		Assert.assertTrue("bola".equalsIgnoreCase("Bola"));
+		/*para string comparar com radicais, ou seja, iniciais das palavras */
+		Assert.assertTrue("bola".startsWith("bo"));
+		/*para comparar objetos, temos que criar um metodo equals na classe dele
+		 * porque senao o Assert vai comparar com o equal do Objeto do proprio Java onde passa o id
+		 * do objeto como comparacao e dara sempre falso
+		 * vamos pra classe usuario e criar esse metodo equals*/
+		Usuario u1 = new Usuario("fulano");
+		Usuario u2 = new Usuario("fulano");
+		Assert.assertEquals(u1, u2);
+	}
+
+}
