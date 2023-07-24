356c3b671a74cb9c35c5f740dc01039b38d4aedd
Erick Alan0123 <erickalan068@gmail.com>
2023-07-24 13:49:04 -0300

    feat(JUinit): aplicando assumption no teste unitario

--- src/test/java/br/ce/wcaquino/servicos/LocacaoServiceTest.java
+++ src/test/java/br/ce/wcaquino/servicos/LocacaoServiceTest.java
@@ -8,11 +8,13 @@ import static org.junit.Assert.fail;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
 
 import org.junit.After;
 import org.junit.Assert;
+import org.junit.Assume;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Rule;
@@ -44,6 +46,9 @@ public class LocacaoServiceTest {
 	
 	@Test
 	public void deveAlugarFilme() throws Exception {
+		//faz com que esse teste nao seja executado no sabado
+		Assume.assumeFalse(DataUtils.verificarDiaSemana(new Date(), Calendar.SATURDAY));
+		
 		//cenario
 		Usuario user = new Usuario("Fulano");
 		List<Filme> filmes = Arrays.asList(new Filme("Filme 1", 2, 5.0));
@@ -149,13 +154,31 @@ public class LocacaoServiceTest {
 											new Filme("Filme 3", 2, 4.0),	
 											new Filme("Filme 4", 2, 4.0),		
 											new Filme("Filme 5", 2, 4.0),		
-											new Filme("Filme 5", 2, 4.0));		
+											new Filme("Filme 6", 2, 4.0));		
 		//acao
 		Locacao resultado = locacaoService.alugarFilme(usuario, filmes);
 		
 		//verificacao
 		// 4+4+(25% 4) + (50% 4) + (75% 4) + (100% 4) = 4+4+3+2+1+0=14
 		assertThat(resultado.getValor(), is(14.0));
+	}
+	
+	@Test
+	public void deveDevolverFilmeNaSegundaAoAlugarNoSabado() throws FilmeSemEstoqueException, LocadoraException {
+		
+		//faz com que esse teste seja executado somente no sabado
+		Assume.assumeTrue(DataUtils.verificarDiaSemana(new Date(), Calendar.SATURDAY));
+		
+		//cenario
+		Usuario usuario = new Usuario("Fulano");
+		List<Filme> filmes = Arrays.asList(new Filme("Filme 1", 4, 5.0));
+		
+		//acao
+		Locacao locacao = locacaoService.alugarFilme(usuario, filmes);
+		
+		//verificacao
+		boolean ehSegunda = DataUtils.verificarDiaSemana(locacao.getDataRetorno(), Calendar.MONDAY);
+		Assert.assertTrue(ehSegunda);
 	}
 	
 	
